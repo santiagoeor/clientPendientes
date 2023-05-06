12371b96e871917f352fd26ecbb6a27292c8e830
Santiago Orozco <everardoorozcocarmona@gmail.com>
2023-05-06 10:43:03 -0500

    termine el componente de eliminar pendientes

--- src/app/pendientes/pages/eliminar-pendiente/eliminar-pendiente.component.ts
+++ src/app/pendientes/pages/eliminar-pendiente/eliminar-pendiente.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Pendientes } from '../../interfaces/pendientes.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PendientesService } from '../../services/pendientes.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-eliminar-pendiente',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./eliminar-pendiente.component.css']
 })
 export class EliminarPendienteComponent {
+
+  loading:boolean = false;
+  pendient!: Pendientes;
+
+  constructor(
+    private activateRoute:ActivatedRoute,
+    private pendienteService:PendientesService,
+    private router: Router
+  ){}
+
+  ngOnInit():void {
+    this.activateRoute.params
+      .pipe(
+        switchMap(({ id }) => this.pendienteService.getPendientesPorId(id))
+      )
+      .subscribe((pendient) => {
+        this.pendient = pendient;
+      });
+  }
+
+  deletePendiente(){
+    this.loading = true;
+    this.pendienteService.delete(this.pendient.pend).subscribe(response => {
+      // console.log(response);
+      this.loading = false;
+      this.router.navigate(['/pendientes']);
+    })
+
+  }
 
 }
